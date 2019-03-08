40b29eaa4a74ae03c6f1e08dc9b2724f7cf3089f
EnTou <915543195@qq.com>
2019-03-08 17:22:14 +0800

    1. Update 2. Commit and Push modify

--- 0308.sql
+++ 0308.sql
@@ -18,7 +18,8 @@ select *
 from scott.emp;
 
 
-select EMPNO, HIREDATE -- 投影
+select EMPNO,
+       HIREDATE -- 投影
 from scott.emp;
 
 select distinct JOB
@@ -29,7 +30,8 @@ from scott.emp
      # where SAL > 2000;
      # where SAL <> 3000;
      # where SAL <> 3000 and JOB <> 'salesman';
-where SAL <> 3000 or JOB <> 'salesman';
+where SAL <> 3000
+   or JOB <> 'salesman';
 
 -- ci: case insensitive
 select *
@@ -42,7 +44,7 @@ from scott.emp
      # order by ENAME desc; -- descend       降序
      # order by ENAME;
      # order by HIREDATE; -- 工龄长的在前？后？
-order by JOb, ENAME desc ;
+order by JOb, ENAME desc;
 
 
 select comm
@@ -51,7 +53,8 @@ order by COMM;
 
 select *
 from scott.emp
-limit 3 offset 0; -- offset 偏移量 可读性强
+limit 3 offset 0;
+-- offset 偏移量 可读性强
 # limit 0, 3; -- 不建议
 
 -- limit 一般位于 order by 之后
@@ -67,11 +70,11 @@ where ENAME not like 'a%s'; -- 不以a开头，或不以s结尾的员工姓名
 
 select ENAME
 from scott.emp
-where ENAME  like '%COTT';
+where ENAME like '%COTT';
 
 select ENAME
 from scott.emp
-where ENAME  like '_COTT'; -- '_':一个任意字符
+where ENAME like '_COTT'; -- '_':一个任意字符
 
 
 select ENAME
@@ -82,19 +85,20 @@ from scott.emp
 where ENAME like '%_%'; -- 查到所有的员工名
 
 update scott.emp
-set ENAME = 'sco_tt' where ENAME = 'scott';
+set ENAME = 'sco_tt'
+where ENAME = 'scott';
 
 select *
 from scott.emp;
 
 select *
 from scott.emp
-# where JOB = 'salesman' or JOB = 'manager' or JOB = 'president';
+     # where JOB = 'salesman' or JOB = 'manager' or JOB = 'president';
 where JOB in ('salesman', 'manager', 'president');
 
 select *
 from scott.emp
-# where SAL >= 2000 and SAL <= 3000; -- [2000,3000]
+     # where SAL >= 2000 and SAL <= 3000; -- [2000,3000]
 where SAL between 2000 and 3000;
 
 select ENAME as '员工姓名' -- as： alias 取别名
@@ -111,10 +115,52 @@ from scott.emp
 where COMM is not null; -- 判非空
 
 update scott.emp
-set COMM = null  -- 空值设置用 = 号
+set COMM = null -- 空值设置用 = 号
 where ENAME = '7499'; -- DML
 
-select ENAME, SAL + ifnull(COMM, 0) -- 奖金不为空： 工资+奖金； 奖金为空：工资+0
+select ENAME,
+       SAL + ifnull(COMM, 0) -- 奖金不为空： 工资+奖金； 奖金为空：工资+0
 from scott.emp;
+
+
+select *
+from scott.emp;
+
+select *
+from scott.dept;
+
+select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
+from scott.emp e
+       inner join scott.dept d -- 可以省略 inner        cross join：交叉连接
+                  on e.DEPTNO = d.DEPTNO;
+-- 少了king
+-- 52 = 13 * 4
+
+-- 在内连接的基础上，显示落选信息：
+select e.ENAME, d.DNAME
+from scott.emp e
+       left outer join scott.dept d -- 可以省略 outer
+                       on e.DEPTNO = d.DEPTNO;
+select e.ENAME, d.DNAME
+from scott.dept d
+            right join scott.emp e
+                       on e.DEPTNO = d.DEPTNO; -- KING
+
+select e.ENAME, d.DNAME
+from scott.dept d
+       right join scott.emp e
+                  on e.DEPTNO = d.DEPTNO -- KING
+union  -- all
+select e.ENAME, d.DNAME
+from scott.emp e
+       right join scott.dept d
+                  on e.DEPTNO = d.DEPTNO; -- OPERATIONS
+
+select e.ENAME, e.DEPTNO, d.DNAME, d.DEPTNO
+from scott.emp e
+       join scott.dept d
+#                           on e.DEPTNO = d.DEPTNO;
+            using (deptno); -- 主外键相同可以用using
+
 
 
