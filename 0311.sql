4ebc94b74021b263bc7294bb77aa7dab5ff4c5f5
EnTou <915543195@qq.com>
2019-03-11 11:14:55 +0800

    1. Update 2. Commit and Push modify

--- 0311.sql
+++ 0311.sql
@@ -40,3 +40,58 @@ from scott.emp e
 select *
 from scott.v_name
 where ENAME = 'allen';
+
+-- install
+-- conf
+-- basic
+-- dialect
+
+-- sal 大于 scott 员工 sal?
+select *
+from scott.emp
+where ENAME = 'scott';
+
+select *
+from scott.emp
+where sal > (
+  select SAL
+  from scott.emp
+  where ENAME = 'scott'
+); -- sub query 子查询
+
+start transaction;
+-- DMLs
+delete
+from scott.emp
+where ENAME = 'jones';
+
+savepoint a;  -- 设置保留点
+
+update scott.emp
+set SAL = 2000
+where name = 'martin';
+
+savepoint b;
+
+delete
+from scott.emp;
+
+select *
+from scott.emp;
+
+rollback to b;  -- 结束事务？ 不结束
+rollback to a;
+
+rollback; -- 回滚 -- 回到事务开始之初
+commit;
+-- 提交 -- 结束事务
+
+-- DDL 可以隐式提交一次事务 create drop alter
+create table scott.test
+(
+  if int
+);
+
+set autocommit = 0; -- 不自动提交
+set autocommit = 1; -- 默认 自动提交
+
